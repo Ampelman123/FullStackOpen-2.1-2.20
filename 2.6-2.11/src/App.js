7183cc637fe99ea92dd5f13de7284ec4fcca221d
Hagen Carstensen <hagen.carstensen@porsche.de>
2021-07-06 12:02:40 +0200

    solve 2.8

--- 2.6-2.11/src/App.js
+++ 2.6-2.11/src/App.js
@@ -2,32 +2,46 @@ import React, { useState } from 'react'
 
 const Person = ({ person }) => {
   return (
-    <li>{person.name}</li>
+    <div>
+      <li>{person.name} {person.number}</li>
+      
+    </div>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas" }
+    {
+      name: "Arto Hellas",
+      number: "069-420-123456"
+    }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
-    console.log(newName)
     setNewName(event.target.value)
 
   }
+
+  const addNumber = (event) => {
+    setNewNumber(event.target.value)
+
+  }
+
   const addPerson = (event) => {
     event.preventDefault()
     const perObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
-    let arr = persons.map(y=>y.name)
+    let arr = persons.map(y => y.name)
     if (arr.includes(newName)) {
       window.alert(`${newName} is already in your phonebook!`);
     } else {
       setPersons(persons.concat(perObject))
       setNewName('')
+      setNewNumber('')
     }
 
 
@@ -41,9 +55,13 @@ const App = () => {
           name: <input value={newName} onChange={addName} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={addNumber} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
       <ul>
         {persons.map(person =>
