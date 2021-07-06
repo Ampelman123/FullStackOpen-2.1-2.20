22a03013db5865f1835f850676a6aa494c5b6bf8
Hagen Carstensen <hagen.carstensen@porsche.de>
2021-07-06 13:40:41 +0200

    solve 2.9

--- 2.6-2.11/src/App.js
+++ 2.6-2.11/src/App.js
@@ -3,21 +3,32 @@ import React, { useState } from 'react'
 const Person = ({ person }) => {
   return (
     <div>
-      <li>{person.name} {person.number}</li>
-      
+      <li>{person.name} {person.number}</li>    
+    </div>
+  )
+}
+const Persons = ({persons, search})=>{
+  let arr = persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase())||person.number.includes(search))
+
+  return (
+    <div>
+      {arr.map(person =>
+          <Person key={person.name} person={person} />
+        )}
     </div>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: "Arto Hellas",
-      number: "069-420-123456"
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] =useState('')
 
   const addName = (event) => {
     setNewName(event.target.value)
@@ -28,7 +39,9 @@ const App = () => {
     setNewNumber(event.target.value)
 
   }
-
+  const addSearch =(event) =>{
+    setSearch(event.target.value)
+  }
   const addPerson = (event) => {
     event.preventDefault()
     const perObject = {
@@ -49,7 +62,11 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        search: <input value={search} onChange={addSearch}></input>
+      </div>
+      <h2>Add new contact</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={addName} />
@@ -63,11 +80,7 @@ const App = () => {
       </form>
 
       <h2>Numbers</h2>
-      <ul>
-        {persons.map(person =>
-          <Person key={person.name} person={person} />
-        )}
-      </ul>
+      <Persons persons={persons}search={search} />
     </div>
   )
 }
